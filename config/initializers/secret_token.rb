a169f7212cb6ebb0aece145f109f33bc56d32ba6
Zach Bryan <zachx1@gmail.com>
2013-07-14 11:34:06 -0700

    Created application using Refinery CMS 2.0.10

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lurc::Application.config.secret_token = 'cbc6a48c11d2cb7e349d553b13aabad2eea686ad1d64f6e2aad3fa64c8eae52647faafb09f57daed920db2f58f799604ad96a3bdd22335c0dbf1b7ee984b5b04'
